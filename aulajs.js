9f4a04abc061c685ab3caa0af5927423407adc72
Andrelpk <andrell_lacerda@hotmail.com>
2021-05-05 19:49:38 -0300

    atualização

---
+++ aulajs.js
@@ -0,0 +1,18 @@
+
+var nome = "André"; // string
+var idade = 28; // Number -> integer (int)
+var peso = 45.6; // Number -> float
+var acordado = true; // boolean
+
+// alert ("Olá " + nome +" Seja bem-vindo");
+
+console.log(nome, idade, peso, acordado)
+
+// criando a função
+function exibe()
+{
+    console.log("Dentro da função");
+    alert("vc chamou a função");
+}
+//executando a função
+//exibe();
